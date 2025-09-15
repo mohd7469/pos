1d5616a45341047d611be9f5e96ae485c08ad988
mohammad <awais3227443@gmail.com>
2025-09-15 19:12:17 +0400

    input type search

--- src/components/FilterControls.jsx
+++ src/components/FilterControls.jsx
@@ -101,6 +101,7 @@ const FilterControls = ({ orders, stores, onFilterChange, filteredCount, screenO
                 <div className="relative flex-grow">
                     <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
                     <Input
+                        type={'search'}
                         placeholder="Search all order fields..."
                         value={searchTerm}
                         onChange={(e) => setSearchTerm(e.target.value)}
